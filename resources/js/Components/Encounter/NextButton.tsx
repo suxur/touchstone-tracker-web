1f1210b1fd4fb7ee732e93b41dcad6c0b5acf6db
Josh Payne <josh.payne@publicstrategies.com>
2022-06-12 20:43:32 -0500

    Clear Actions on New Round

--- resources/js/Components/Encounter/NextButton.tsx
+++ resources/js/Components/Encounter/NextButton.tsx
@@ -1,18 +1,39 @@
-import * as React from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import * as React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import { useEncounter } from '@/Hooks/useEncounter';
-import { JetButton } from '@/Components/Jetstream';
+import { useEncounter } from "@/Hooks/useEncounter";
+import { JetButton } from "@/Components/Jetstream";
 
 export const NextButton = () => {
   const { encounter, mutation } = useEncounter();
 
+  const isNewRound = () => {
+    if (!encounter) return false;
+
+    return encounter.active_index === encounter.combatants.length - 1;
+  };
+
   const onClick = () => {
     if (encounter) {
-      if (encounter.active_index === encounter.combatants.length - 1) {
-        mutation.mutate({ ...encounter, round: encounter.round + 1, active_index: 0 });
+      if (isNewRound()) {
+        const combatants = encounter.combatants.map((combatant) => ({
+          ...combatant,
+          action: false,
+          bonus_action: false,
+          reaction: false,
+        }));
+
+        mutation.mutate({
+          ...encounter,
+          round: encounter.round + 1,
+          active_index: 0,
+          combatants,
+        });
       } else {
-        mutation.mutate({ ...encounter, active_index: encounter.active_index + 1 });
+        mutation.mutate({
+          ...encounter,
+          active_index: encounter.active_index + 1,
+        });
       }
     }
   };
